6fa51261c5863539ef336e007eb5c80c38b71f78
DevFaith <waitherafaith@outlook.com>
2024-07-22 12:55:09 +0300

    * feat(View.jsx): add new View component with an iframe to display 'https://www.buniflow.com/'

---
+++ src/pages/View.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <div>
+      <iframe src='https://www.buniflow.com/'
+      className='w-full h-screen'
+
+      ></iframe>
+    </div>
+  )
+}
+
+export default View
